33e05d2a375a2e0c39f2c1d02471241ab8a84fce
jerny <jerny@ubuntu.(none)>
2011-03-11 23:43:57 -0600

    still problems with commit?

--- deploy/populateportfolio.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `PORTFOLIOSYMBOLS` ( `persistId` , `symbol` ) VALUES ('0', 'ZB');
-INSERT INTO `PORTFOLIOSYMBOLS` ( `persistId` , `symbol` ) VALUES ('1', 'OZB');
-INSERT INTO `PORTFOLIOSYMBOLS` ( `persistId` , `symbol` ) VALUES ('2', 'ZF');
-INSERT INTO `PORTFOLIOSYMBOLS` ( `persistId` , `symbol` ) VALUES ('3', 'OZF');
-INSERT INTO `PORTFOLIOSYMBOLS` ( `persistId` , `symbol` ) VALUES ('4', 'ZT');
-INSERT INTO `PORTFOLIOSYMBOLS` ( `persistId` , `symbol` ) VALUES ('5', 'OZT');
--- deploy/populatetraders.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO TRADERS (`persistId`, `traderUsername`, `password`, `passwordCreationDate`) VALUES (1, 'support', 'iliketrading', NOW());
-INSERT INTO TRADERS (`persistId`, `traderUsername`, `password`, `passwordCreationDate`, `accountType`) VALUES (2, 'OCAPI', 'ocapi', NOW(), 2);
-INSERT INTO TRADERS (`persistId`, `traderUsername`, `password`, `passwordCreationDate`, `isDefault`) VALUES (3, 'CHRIS', 'moag', NOW(), 1);
-INSERT INTO TRADERS (`persistId`, `traderUsername`, `password`, `passwordCreationDate`, `isDefault`) VALUES (4, 'JEFF', 'melgarde', NOW(), 0);
